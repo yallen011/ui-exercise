21b5edb0933b2a76f3a140c578b8d97a4d329868
Yvonne Allen <yallen011@gmail.com>
2020-03-20 04:48:59 -0400

    remove angular starter code and added initial app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,3 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'ui-exercise';
-}
+import { Component } from '@angular/core'; @Component({ selector: 'app-root',
+templateUrl: './app.component.html', styleUrls: ['./app.component.scss'] })
+export class AppComponent { title = 'Gmail Clone '; }
